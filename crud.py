08c375811e799f1e0a5035b841d31c7cf7a9287e
vijaylaxmi hanamshetty <vijaylaxmihanamshetty38@.in>
2024-11-14 10:08:13 +0530

    add the schema for access token and and the crud for create user

--- crud.py
+++ crud.py
@@ -3,6 +3,16 @@ import models
 import schema
 from fastapi import HTTPException
 
+# Create User
+def create_user(db: Session, user: schema.UserCreate, hashed_password: str):
+    db_user = models.User(username=user.username, hashed_password=hashed_password, role=user.role)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
 # Create Post
 def create_post(db: Session, post: schema.PostCreate, owner_id: int):
     db_post = models.Post(**post.dict(), owner_id=owner_id)
